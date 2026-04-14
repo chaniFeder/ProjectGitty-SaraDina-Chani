b52da3136344e4dc0e035972889ccf7c856b007b
chaniFeder <hand3e12@mbjcomp.org.il>
2026-04-14 14:54:36 +0300

    Bl implements

---
+++ Project/Bl/Api/IAdminApi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl.Api
+{
+    internal interface IAdminApi
+    {
+        bool Login(string username, string password);
+        string GetSystemStatistics();
+        IEnumerable FilterCases(FilterCriteria criteria);
+        bool EditCase(int caseId, CaseEditDetails details);
+    }
+}
