a0ef93f58c3660d65235bf7dc5f057b4cc7571d7
רובין גיטי < @mbjcomp.local>
2026-04-14 15:56:05 +0300

    implement services

--- Project/Bl/Services/AdminService.cs
+++ Project/Bl/Services/AdminService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bl.Api;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace Bl.Services
 {
-    internal class AdminService
+    internal class AdminService : IAdminApi
     {
+        public bool EditCase(int caseId, CaseEditDetails details)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable FilterCases(FilterCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSystemStatistics()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Project/Bl/Services/CustomerService.cs
+++ Project/Bl/Services/CustomerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bl.Api;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,26 @@ using System.Threading.Tasks;
 
 namespace Bl.Services
 {
-    internal class CustomerService
+    internal class CustomerService : ICustomerApi
     {
+        public bool CreateNewCase(CaseDetails details)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CaseDetails GetDashboardData(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Register(CustomerDetails details)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
