16a2f43df6cdef0531cd9527d2e70bee2a45a5f2
chaniFeder <hand3e12@mbjcomp.org.il>
2026-05-03 13:14:37 +0300

    implement interfice

--- Project/Bl/Models/Admin/BankDto.cs
+++ Project/Bl/Models/Admin/BankDto.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Bl.Models.Admin
 {
-    internal class BankDto
+    public class BankDto
     {
+<<<<<<< Updated upstream
+=======
+        public string BankName { get; set; }
+
+        public int BankCode { get; set; }
+
+        public string? ContactPerson { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; } = null!;
+
+        public bool? IsActive { get; set; }
+
+        public double? MinLoanAmount { get; set; }
+
+        public double? MaxLoanAmount { get; set; }
+>>>>>>> Stashed changes
     }
 }
